a20bf83061e81c3fd71f0c565a59aab5b4180802
Curyeh <jonas.helling4@gmail.com>
2025-03-11 12:22:30 +0100

    Made changes from String/long to int so variables match in noti_freq_for_task

--- Spring_JPA_N-g/src/main/java/com/nag/spring_jpa_ng/controller/Noti_freq_for_taskController.java
+++ Spring_JPA_N-g/src/main/java/com/nag/spring_jpa_ng/controller/Noti_freq_for_taskController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 
-// Claimed by Jonas
+// Claimed by Jonas.
 public class Noti_freq_for_taskController {
 
     @Autowired
--- Spring_JPA_N-g/src/main/java/com/nag/spring_jpa_ng/repository/Noti_freq_for_taskRepository.java
+++ Spring_JPA_N-g/src/main/java/com/nag/spring_jpa_ng/repository/Noti_freq_for_taskRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.nag.spring_jpa_ng.model.Noti_freq_for_task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Claimed by Jonas
 public interface Noti_freq_for_taskRepository extends JpaRepository<Noti_freq_for_task, Long> {
     List <Noti_freq_for_task> findByTaskId(int taskId);
     List <Noti_freq_for_task> findByNotiFreqId(int notiFreqId);
