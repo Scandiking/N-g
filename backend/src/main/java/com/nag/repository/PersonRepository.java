a59057700570f8092241b2090304eb24e5dd5b6e
JonasElH <jonas.helling4@gmail.com>
2025-05-28 10:40:16 +0200

    Added model for Person and repository for person.

---
+++ backend/src/main/java/com/nag/repository/PersonRepository.java
@@ -0,0 +1,13 @@
+package com.nag.repository;
+
+import com.nag.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Repository interface for managing Person entities.
+ * This interface extends JpaRepository to provide CRUD operations for Person entities.
+ */
