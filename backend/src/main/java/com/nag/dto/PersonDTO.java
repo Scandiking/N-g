9d1ea1a95de5fc38213d8fafb1468fc661dd7a6f
Kristian Martin Tvenning <km.tvenning@protonmail.com>
2025-05-28 14:08:14 +0200

    Created DTO classes for Task, Room, Person and NotiFreq with JavaDoc annotations. No manual setters or getters because of the annotations.

---
+++ backend/src/main/java/com/nag/dto/PersonDTO.java
@@ -0,0 +1,28 @@
+package com.nag.dto;
+
+/**
+ * Data Transfer Object (DTO) for Task.
+ * This class is used to transfer task data between different layers of the application.
+ *
+ * @author Kristian
+ */
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor // Lombok will generate a no-args constructor
+@AllArgsConstructor // Lombok will generate a constructor with all fields
+public class PersonDTO {
+    private String phoneNo;
+    private String firstName;
+    private String lastName;
+    private String mail;
+    private byte[] profilePicture; // This is a byte array for the profile picture
+    private LocalDate birthDate;
+
+    // No need for setters or getters as Lombok's @Data annotation generates them automatically
+}
---
+++ backend/src/main/java/com/nag/dto/RoomDTO.java
@@ -0,0 +1,26 @@
+package com.nag.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Data Transfer Object (DTO) for Task.
+ *     This class is used to transfer task data between different layers of the application.
+ *
+ * @author Kristian
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoomDTO {
+    private Short roomId;
+    private String roomName;
+    private String roomDescr;
+    private String roomAdmin;
+    private byte[] roomPicture;
+
+    // No need for setters or getters as Lombok's @Data annotation generates them automatically
+
+}
