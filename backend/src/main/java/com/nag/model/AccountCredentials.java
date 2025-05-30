fc548a556298d4c828f529e7a1f55f7429451bc4
Kristian Martin Tvenning <km.tvenning@protonmail.com>
2025-05-30 14:15:35 +0200

    Opprettet filer som beskrevet i Hinkula

---
+++ backend/src/main/java/com/nag/model/AccountCredentials.java
@@ -0,0 +1,9 @@
+package com.nag.model;
+
+/**
+ * @description
+ * @author Hinkula (Kristian)
+ */
+
+public record AccountCredentials(String username, String password)
+{ }
---
+++ backend/src/main/java/com/nag/service/JwtService.java
@@ -0,0 +1,56 @@
+package com.nag.service;
+
+/**
+ * @description
+ * @author Hinkula (Kristian)
+ */
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+
+@Component
+public class JwtService {
+    static final long EXPIRATION_TIME = 86400000; // 1 døgn i millisekunder, burde være kortere i produksjon,
+    static final String PREFIX = "Bearer";
+
+    // Generate secret key. Only for demonstation purposes.
+    // In production, you should read it from the application configuration.
+    static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    // Genereate signed JTW token
+    public String getToken(String username) {
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+
+        return token;
+    }
+
+    // Get a token from request authorization header,
+    // verify the token, and get username
+    public String getAuthUser(HttpServletRequest request) {
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (token != null) {
+            String user = Jwts.parser() // står parserBuilder i boka, men det fungerer med bare parser og ikke med parserBuilder, men parser er deprecated
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token.replace(PREFIX, ""))
+            .getBody()
+            .getSubject();
+
+            if (user != null)
+                return user;
+        }
+        return null;
+    }
+}
