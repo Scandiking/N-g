f5b09af15ec4a1cfa37b7ab271759233aa1ca81a
JonasElH <jonas.helling4@gmail.com>
2025-06-11 18:11:14 +0200

    test

--- backend/src/main/java/com/nag/dto/TaskDTO.java
+++ backend/src/main/java/com/nag/dto/TaskDTO.java
@@ -1,9 +1,11 @@
+// src/main/java/com/nag/dto/TaskDTO.java
 package com.nag.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDate;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -12,7 +14,7 @@ public class TaskDTO {
     private Integer taskId;
     private String title;
     private String description;
-    private LocalDate dueDate;
+    private LocalDateTime dueDate;   // changed from LocalDate
     private Short notiFreqId;
     private String creator;
     private boolean completed;
